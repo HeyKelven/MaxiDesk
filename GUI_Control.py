c45b03aca6671c293deeaf801bdcfe0e2a21eea3
HeyKelven <victork.laerte@gmail.com>
2018-07-02 14:52:38 -0300

    Create function SUCCESS

--- GUI_Control.py
+++ GUI_Control.py
@@ -7,18 +7,26 @@ from tkinter import messagebox
 from win32api import GetSystemMetrics
 import threading
 
+def conclude_call(id):
+    archive = tools.read_csv(tools.path_vendas)
+    for row in archive:
+        if row[3] == id:
+            print(row[1])
 
 def put_label(frame, put_row, count):
     if put_row[4] == "0":
+        v = StringVar()
+        v.set("Pendente")
         Label(frame, text='Solicitante: ' + put_row[0]).grid(row=count, column=0, sticky=W, padx=(5, 10))
-        Label(frame, text='Departamento: ' + put_row[1]).grid(row=count, column=1, sticky=W, padx=(0, 5))
-        Label(frame, text='Problema: ' + put_row[2]).grid(row=count, column=2, sticky=W, padx=(0, 7))
+        Label(frame, text='Departamento: ' + put_row[2]).grid(row=count, column=1, sticky=W, padx=(0, 5))
+        Label(frame, text='Problema: ' + put_row[1]).grid(row=count, column=2, sticky=W, padx=(0, 7))
         Label(frame, text='Data de abertura: ' + put_row[6] + ' ' + row[5]) \
             .grid(row=count, column=3, sticky=W, padx=(7, 7))
         Label(frame, text='Id: ' + put_row[3]).grid(row=count, column=4, sticky=W, padx=(7, 7))
         Entry(frame).grid(row=count, column=5, padx=(0, 7), pady=(3, 3))
         Entry(frame).grid(row=count, column=6, padx=(0, 7), pady=(3, 3))
-        Button(frame, text='OK', state=DISABLED).grid(row=count, column=7, padx=(0, 7), pady=(3, 3))
+        Label(frame, textvariable=v).grid(row=count, column=7, sticky=W, padx=(0, 7))
+        Button(frame, text='OK', state=NORMAL, command=lambda: [v.set("Concluído"), conclude_call(put_row[3])]).grid(row=count, column=8, padx=(0, 7), pady=(3, 3))
 
 
 class App(threading.Thread):
@@ -45,7 +53,7 @@ class App(threading.Thread):
                         if row not in self.data:
                             print(row)
                             self.data.append(row)
-                            putlabel(self.frame, row, self.count)
+                            put_label(self.frame, row, self.count)
                             self.count = + 1
 
     def on_closing(self):
@@ -61,7 +69,7 @@ def down(event):
 
 root = Tk()
 root.title('MaxiDesk')
-root.geometry('1200x600+' + str(int(GetSystemMetrics(0) / 2 - 600)) + '+' + str(int(GetSystemMetrics(1) / 2 - 300)))
+root.geometry('1200x400+' + str(int(GetSystemMetrics(0) / 2 - 600)) + '+' + str(int(GetSystemMetrics(1) / 2 - 200)))
 root.resizable(False, False)
 
 principal_frame = Frame(root).pack(fill=BOTH)
@@ -76,7 +84,7 @@ with open(tools.path_vendas, 'r') as looking_at:
     for row in reader:
         data.append(row)
         if row[4] == "0":
-            putlabel(second_frame, row, i)
+            put_label(second_frame, row, i)
             i += 1
 
 scroll = Scrollbar(principal_frame, orient='vertical', command=canvas.yview)
